64c679637062e9cf693e018427996bb635ed055f
PrasunaDash <prasuna.dash@advanzetech.com>
2026-06-01 18:18:25 +0530

    new files added

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "quotetobind-hxf4euawe9gbf0g3.centralindia-01.azurewebsites.net",
+      "images.unsplash.com"
+    ],
+  },
+};
+
+export default nextConfig;
